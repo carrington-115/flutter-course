de87e72393cd42e465954e9d3a3238072ed5e956
carrington-115 <frumarkcarrington@gmail.com>
2025-03-09 23:16:11 +0530

    learned how the widget tree works in practice learned how to work with types in flutter and ensure type safety learened how to add colors, gradients, containers, basic layout, learned how to access multiple configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello world!")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 61, 6, 2),
+                Color.fromARGB(255, 126, 5, 1),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello world! How are you?",
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
