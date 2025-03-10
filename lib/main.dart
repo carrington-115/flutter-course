90ef26bb4381bcb09f027ca87434d40f1218f4a6
carrington-115 <frumarkcarrington@gmail.com>
2025-03-11 02:02:00 +0530

    built a custom widget that has been used to extract a section of the code into a class and make it less complicated and organized

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 61, 6, 2),
-                Color.fromARGB(255, 126, 5, 1),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello world! How are you?",
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+// custom widget
+
+class GradientContainer extends StatelessWidget {
+  @override // override all the features from Gradient
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 61, 6, 2),
+            Color.fromARGB(255, 126, 5, 1),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          "Hello world! How are you?",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
 }
